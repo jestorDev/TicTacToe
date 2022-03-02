4252ffc3d36098c3db3e473340aa932c87d8f26e
jestorDev <torrele.09@gmail.com>
2022-03-02 18:53:58 -0500

    Completed Basic Table

--- app/src/main/java/com/example/tictactoecompose/MainActivity.kt
+++ app/src/main/java/com/example/tictactoecompose/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.tictactoecompose
 
 import android.graphics.ColorFilter
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -23,6 +21,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tictactoecompose.ui.theme.TicTacToeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,16 +66,37 @@ fun strNew(prev: String , i : Int , j : Int):String{
     return prev.substring(0, idx) + charNext(prev[idx]) + prev.substring(idx + 1)
 }
 
+fun checkWinner(tb: String):Char{
+    for (c in charArrayOf('X' , 'O')){
+        
+        if (tb[0] == c && tb[4] == c && tb[8] == c)
+            return c
+        if (tb[2] == c && tb[4] == c && tb[6] == c)
+            return c         
+        for (i in 0 until  3){
+            if (tb[0 + i*3] == c && tb[1+ i*3] == c && tb[2+ i*3] == c)
+                return c
+            if (tb[0 + i] == c && tb[3+ i] == c && tb[6+ i] == c)
+                return c
+        }
+        
+    }
+    return 'B'
+
+}
 
 @Preview(showBackground = true)
 @Composable
 fun Table() {
 
     var tb = rememberSaveable{ mutableStateOf("BBBBBBBXO") }
-    //var winner = rememberSaveable{ mutableStateOf("B") }
+    var winner = rememberSaveable{ mutableStateOf('B') }
     Column(
-        Modifier.background(Color.DarkGray).fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        Modifier
+            .background(Color.DarkGray)
+            .fillMaxWidth().fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,verticalArrangement =Arrangement.Center
+
 
 
     ) {
@@ -86,12 +106,18 @@ fun Table() {
                 for (j in 0 until 3){
                     Image(
                         painter = painterResource(id = charImg(tb.value[i*3+j])) ,
-                        contentDescription = "sq", modifier = Modifier.size(110.dp).clickable {
-                            tb.value = strNew(tb.value , i , j) }
+                        contentDescription = "sq", modifier = Modifier
+                            .size(110.dp)
+                            .clickable {
+                                tb.value = strNew(tb.value, i, j)
+                                winner.value = checkWinner(tb.value)
+                            }
                     )
                 }
             }
         }
+        
+        Text(text = "The winner is " + winner.value, color = Color.LightGray , fontSize = 40.sp)
 
     }
 
